a472a9377ee4660887541689757d2c484b98a0cd
Neel Smith <dnsmith.neel@gmail.com>
2021-05-31 16:06:15 -0400

    Work on corpus profile

--- data/vocabnotes.jl
+++ data/vocabnotes.jl
@@ -1,16 +1,33 @@
+# Use this script from the `data` directory, or else
+# modify the following line to point to the right directory
+# for the root of the repository:
+reporoot = pwd() |> dirname
+
+
+using EditorsRepo
+# using CitableCorpus # After Thurs!
+using CitableText
+using ManuscriptOrthography
+using Orthography
+using FreqTables
 
 repo = EditingRepository(reporoot, "editions", "dse", "config")
+scholiaurn = CtsUrn("urn:cts:greekLit:tlg5026.e3.hmt:")
 #(Find all URNs in repo if you're not sure)
-txturns = texturns(repo)
+# txturns = texturns(repo)
 
 
-# Build a corpus for a given urn from a repo:
-c = normalizednodes(repo, u) |> CitableCorpus
+# Collect normalized nodes and filter for "comment" nodes
+nodes = normalizednodes(repo, scholiaurn) 
+comments = filter(cn -> endswith(passagecomponent(cn.urn), "comment"), nodes)
+
 
 #Use MS orthography and tokenize corpus:
 orth = msGreek()
-tkns = map(cn -> tokenize(orth, cn.text), c.corpus) |> Iterators.flatten |> collect
+tkns = map(cn -> tokenize(orth, cn.text), comments) |> Iterators.flatten |> collect
+
 
 # Select lexical tokens, and extract text content:
 lex = filter(t ->  t.tokencategory == LexicalToken(), tkns)
+avglen = length(lex) ÷ length(comments)
 tstrs = map(l -> l.text, lex)
